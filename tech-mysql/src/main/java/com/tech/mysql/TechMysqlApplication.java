935e40f3eb24df772c393cb3c0f58acade9d6146
mingliangwen <mingliangwen@zhidaoauto.com>
2023-06-25 15:24:04 +0800

    init mysql

---
+++ tech-mysql/src/main/java/com/tech/mysql/TechMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.tech.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechMysqlApplication.class, args);
+    }
+
+}
