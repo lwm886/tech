a6f9d456e71e1c4ce8d6d98e4375269a733a25c1
mingliangwen <mingliangwen@zhidaoauto.com>
2023-07-28 08:31:49 +0800

    init test

---
+++ tech-test/src/main/java/com/tech/test/mode/singleton/Singleton.java
@@ -0,0 +1,18 @@
+package com.tech.ot.mode.singleton;
+
+/**
+ * @author lw
+ * @since 2023/7/5
+ */
+public class Singleton {
+    private Singleton() {
+    }
+
+    private static class LazyHolder {
+        final static Singleton SINGLETON = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return LazyHolder.SINGLETON;
+    }
+}
