c8cb6440f050d0bce5edefedd484dad7cbf1a3da
mingliangwen <mingliangwen@zhidaoauto.com>
2023-07-28 08:28:33 +0800

    Singleton

---
+++ tech-ot/src/main/java/com/tech/ot/TechOtApplication.java
@@ -0,0 +1,13 @@
+package com.tech.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechOtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechOtApplication.class, args);
+    }
+
+}
