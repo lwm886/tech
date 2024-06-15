0f9129b107a09751bb8f06721a8592dddc19fe34
mingliangwen <admin@qq.com>
2024-06-15 19:01:04 +0800

    add kafka

---
+++ tech-kafka/tech-kafka-basic/src/main/java/com/tech/kafka/TechKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.tech.techkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechKafkaApplication.class, args);
+    }
+
+}
---
+++ tech-kafka/tech-kafka-basic/src/test/java/com/tech/techkafka/TechKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.techkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
