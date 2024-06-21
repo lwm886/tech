9a38015ea06a65f3cfcc0346535e57f3635dadbd
lw <admin@qq.com>
2024-06-21 13:42:45 +0800

    boot consume msg

---
+++ tech-kafka/tech-kafka-boot/src/main/java/com/tech/kafka/TechKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.tech.kafka;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechKafkaApplication.class, args);
+    }
+}
