aa1946167ae9ba234407ae3fa0a9137efa7ddc3b
lw <admin@qq.com>
2024-07-27 14:38:50 +0800

    init sharding-jdbc

---
+++ tech-sharding-sphere/tech-sharding-jdbc/src/main/java/com/tech/sj/basic/TechShadingJDBCApplication.java
@@ -0,0 +1,13 @@
+package com.tech.sj.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechShadingJDBCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechShadingJDBCApplication.class, args);
+    }
+
+}
---
+++ tech-sharding-sphere/tech-sharding-jdbc/src/test/java/com/tech/sj/basic/TechShadingJDBCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.sj.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShadingJDBCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
