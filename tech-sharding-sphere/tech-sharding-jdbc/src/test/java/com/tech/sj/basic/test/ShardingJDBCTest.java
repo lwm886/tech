427d06cb86a3241438a875a31c259b567ae48c15
lw <admin@qq.com>
2024-07-29 20:42:19 +0800

    shardingJDBC 分库分表 Sample 写入和查询

--- tech-sharding-sphere/tech-sharding-jdbc/src/test/java/com/tech/sj/basic/test/ShardingJDBCTest.java
+++ tech-sharding-sphere/tech-sharding-jdbc/src/test/java/com/tech/sj/basic/test/ShardingJDBCTest.java
@@ -1,5 +1,6 @@
 package com.tech.sj.basic.test;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.tech.sj.basic.entity.Course;
 import com.tech.sj.basic.entity.SjCourse;
 import com.tech.sj.basic.mapper.CourseMapper;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import javax.xml.bind.annotation.W3CDomHandler;
+import java.util.List;
 
 /**
  * @author lw
@@ -26,11 +29,21 @@ public class ShardingJDBCTest {
     public void addCourse(){
         for (int i = 0; i < 10; i++) {
             Course c = new Course();
-            c.setCid(Long.valueOf(i));
+//            c.setCid(Long.valueOf(i));
             c.setCname("shardingSphere");
             c.setUserId(Long.valueOf(1000+Long.valueOf(i)));
             c.setCstatus("1");
             courseMapper.insert(c);
         }
     }
+
+    @Test
+    public void queryCourse(){
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("cid");
+        wrapper.eq("cid",1L); //根据分片键直接定位库和表
+        List<Course> courses = courseMapper.selectList(wrapper);
+        courses.forEach(System.out::println);
+    }
+    
 }
